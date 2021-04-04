6b36ea57980b7bd1d59259d990cd0d27cdfdf438
realdragonhead <offend1ng02007@gmail.com>
2021-04-04 17:42:51 +0900

    01 report

---
+++ app.js
@@ -0,0 +1,90 @@
+console.log('app.js');
+
+// var name = "진용두";
+// var Name = "JinYongdoo"
+// var age = 28;
+// var message = "Hello "+ Name;
+
+// console.log(name);
+// console.log(Name);
+// console.log(message);
+
+// 주석
+// console.log(1);
+// console.log(2);
+
+// var count = 1;
+// console.log(count);
+
+// var isMan = true;   //bool
+// var isMan = "true"; //String
+
+// function log(str) {
+//     console.log(str);
+// }
+// log("hello");
+
+// var log = function(str) {
+//     console.log(str);
+// }
+// log("hello");
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+// };
+// console.log(image);
+
+// var imageWidth = 100;
+// var imageHeight = 100;
+// var imageName = '1.png';
+
+// var image = [];
+// image[0] = 100;
+// image[1] = 100;
+// image[2] = '1.png';
+// console.log(image);
+
+// var image = [1, 2, 3, 4, 5, 'a', [1, 2, 3]];
+// image[0] = 0;
+// console.log(image);
+
+// var data = null;    //NULL
+// var data1 = '';
+// var data2;          //undefined
+
+// console.log(data, data1, data2);
+
+// console.log(1 + 2);
+// console.log('a' + 'b');
+// console.log('hello ' + 'world');
+// console.log(1 + 'a');
+
+// == 동등
+// === 일치
+// console.log(1=='1');
+// console.log(1==='1');
+// console.log('' == '0');
+// console.log('' == 0);
+// console.log('' === 0);
+
+// if (true) {
+//     console.log(1);
+// }
+
+// if(!0) {
+//     console.log(1);
+// }
+
+// var Image = {
+//     width: 100,
+//     height: 100,
+//     name : '1.png',
+// };
+// delete Image.width;
+// console.log(Image);
+// console.log(typeof(Image));
+// console.log(typeof(1));
+// console.log(typeof('1'));
+// console.log(typeof([]));
