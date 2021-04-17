ce621b755a386d35583bde177182c429a240604d
realdragonhead <offend1ng02007@gmail.com>
2021-04-18 01:08:56 +0900

    07

--- 07/test.js
+++ 07/test.js
@@ -1,15 +1,6 @@
 const str = 'aaa';
 const obj = {
-  a: 1,
-  b: 1,
-  c: 1,
-  d: 1,
-  e: 1,
-  a: 1,
-  b: 1,
-  c: 1,
-  d: 1,
-  e: 1,
+  // eslintrc.js 에 규칙을 추가해서 에러 제거
   a: 1,
   b: 1,
   c: 1,
