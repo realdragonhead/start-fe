92b04c7d9f75908c01f753b52286894d8b5edf9d
realdragonhead <offend1ng02007@gmail.com>
2021-04-17 23:45:52 +0900

    07

---
+++ 07/test.js
@@ -0,0 +1,19 @@
+const str = 'aaa';
+const obj = {
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+  e: 1,
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+  e: 1,
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+  e: 1,
+};
+console.log(1);
